ba2495896b5be24cfad2325e54bd771b0c1f0704
Varad Gundap <30592638+varad615@users.noreply.github.com>
2023-02-05 16:47:21 +0530

    Login

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,14 @@
+import { Button } from "@nextui-org/react";
 import Link from "next/link";
-import fetch from "isomorphic-unfetch";
-import { Button, Card } from "@nextui-org/react";
 
-const Index = ({ notes }) => {
+function Home() {
+
   return (
-    <div>
-      <Button>Connect Wallet</Button>
-    </div>
+    <>
+      <Link href="/signin">
+      <Button>Signin</Button></Link>
+    </>
   );
-};
+}
 
-Index.getInitialProps = async () => {
-  const res = await fetch("http://localhost:3000/api/notes");
-  const { data } = await res.json();
-
-  return { notes: data };
-};
-
-export default Index;
+export default Home;
---
+++ pages/profile.js
@@ -0,0 +1,11 @@
+import { Navbar, Button, Link, Text } from "@nextui-org/react";
+
+export default function Profile() {
+  return (
+<div>
+    <div>
+        
+    </div>
+</div>
+  );
+}
